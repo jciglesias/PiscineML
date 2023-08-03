805fe3e0a1ced0a47c8b3be073db087dfa84d65b
ic-juank <jiglesia@student.42.fr>
2023-08-03 16:57:37 +0200

    plot

---
+++ ex05/plot.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot(x, y, theta):
+    """Plot the data and prediction line from three non-empty numpy.array.
+    Args:
+    x: has to be an numpy.array, a vector of dimension m * 1.
+    y: has to be an numpy.array, a vector of dimension m * 1.
+    theta: has to be an numpy.array, a vector of dimension 2 * 1.
+    Returns:
+    Nothing.
+    Raises:
+    This function should not raise any Exceptions.
+    """
+    m, b = np.polyfit(x,y,1)
+    tmp = []
+    for i in x:
+        tmp.append(m*i+b)
+    plt.plot(x, tmp)
+    plt.scatter(x, y)
+    plt.show()
