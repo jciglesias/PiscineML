05906d46310ac82324aa3b083c90642dca8db7e9
ic-juank <jiglesia@student.42.fr>
2023-08-04 11:43:23 +0200

    loss function

---
+++ ex06/loss.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def loss_elem_(y, y_hat):
+    """
+    Description:
+    Calculates all the elements (y_pred - y)^2 of the loss function.
+    Args:
+    y: has to be an numpy.array, a vector.
+    y_hat: has to be an numpy.array, a vector.
+    Returns:
+    J_elem: numpy.array, a vector of dimension (number of the training examples,1).
+    None if there is a dimension matching problem between X, Y or theta.
+    None if any argument is not of the expected type.
+    Raises:
+    This function should not raise any Exception.
+    """
+    pass
+def loss_(y, y_hat):
+    """
+    Description:
+    Calculates the value of loss function.
+    Args:
+    y: has to be an numpy.array, a vector.
+    y_hat: has to be an numpy.array, a vector.
+    Returns:
+    J_value : has to be a float.
+    None if there is a dimension matching problem between X, Y or theta.
+    None if any argument is not of the expected type.
+    Raises:
+    This function should not raise any Exception.
+    """
+    pass
